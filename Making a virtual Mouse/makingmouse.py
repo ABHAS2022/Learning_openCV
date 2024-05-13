d317b06922b30e1aa0e9bf3839bea01b3aee32a2
Abhas Pastor <2021uee0124@iitjammu.ac.in>
2024-05-13 23:11:11 +0530

    It will not create glitch in opening GUI window

--- Making a virtual Mouse/makingmouse.py
+++ Making a virtual Mouse/makingmouse.py
@@ -1,77 +1,3 @@
-
-# import handtrackingmodule as htm
-# import cv2
-# import numpy as np
-# import time
-# import pyautogui
-
-# pyautogui.FAILSAFE = False
-
-
-# sensitivity = 4
-# plocX,plocY = 0,0
-# clocX,clocY = 0,0
-
-
-# wCam = 640
-# hCam = 488
-# frameR = 100
-# cap = cv2.VideoCapture(0)
-# cap.set(3,wCam)
-# cap.set(4,hCam)
-# detector = htm.handDetector(maxHands=1)
-# wScr,hScr = pyautogui.size()
-# # print(wScr,hScr)
-# while True:
-
-
-#     #-->1 handLandmark
-#     frame, img = cap.read()
-#     img = detector.findhands(img=img)
-#     lmlist,bbox = detector.findposition(img=img)
-#     # print(lmlist)
-#     #-->2 getting indexfinger and thumb index
-#     # print(lmlist)
-#     if(len(lmlist)!=0):
-#         # print(lmlist)
-#         x1 , y1 = lmlist[8][1:]
-#         x2 , y2 = lmlist[12][1:]
-#         # print(x1,y1,x2,y2)
-
-#     #-->3 checking number of upped fingers
-#     finger = detector.fingersUp()
-#     # print(finger)
-#     cv2.rectangle(img=img,pt1 = (frameR,frameR),pt2 = (wCam - frameR,hCam - frameR),color=(0,255,0),thickness=4,)
-#     #-->4 only index finger:
-#     if finger[1] == 1 and finger[2] == 0:
-
-#         x3 = np.interp(x1,(frameR,wCam-frameR),(0,wScr))
-#         y3 = np.interp(y1,(frameR,hCam-frameR),(0,hScr))
-#         # x4 = np.interp(x2,(0,wCam),(0,wScr))
-#         # y4 = np.interp(y2,(0,hCam),(0,hScr))
-#         clocX = (plocX) + (x3 - plocX)/sensitivity
-#         clocY = (plocY) + (y3 - plocY)/sensitivity
-#         pyautogui.moveTo(plocX,plocY)
-#         # cv2.circle(img,(x3,y3),5,(255,0,255),cv2.FILLED)
-#         plocX,plocY = clocX,clocY        
-
-#     #reducing the shaking
-
-
-#     #-->clicking 
-#     if finger[1] == 1 and finger[2] == 1:
-#         distance,img,line_info = detector.finddistance(8,12,img)
-#         print(distance)
-#         if distance < 50:
-#             cv2.circle(img,(line_info[4],line_info[5]),15,(255,192,203),cv2.FILLED)
-#             pyautogui.click(x = plocX,y = plocY)
-
-
-#     cv2.imshow("Image",img)
-#     cv2.waitKey(1)
-
-
-
 import cv2
 import numpy as np
 import handtrackingmodule as htm
@@ -94,8 +20,6 @@ cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = pyautogui.size()
 
-# print(wScr, hScr)
-
 while True:
     # Step1: Find the landmarks
     success, img = cap.read()
@@ -114,7 +38,6 @@ while True:
 
         # Step4: Only Index Finger: Moving Mode
         if fingers[1] == 1 and fingers[2] == 0:
-
             # Step5: Convert the coordinates
             x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
@@ -130,7 +53,6 @@ while True:
 
         # Step8: Both Index and middle are up: Clicking Mode
         if fingers[1] == 1 and fingers[2] == 1:
-
             # Step9: Find distance between fingers
             length, img, lineInfo = detector.finddistance(8, 12, img)
 
@@ -147,3 +69,11 @@ while True:
 
     # Step12: Display
     cv2.imshow("Image", img)
+
+    # Step13: Wait for a key press and break loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Step14: Release the capture and close OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
